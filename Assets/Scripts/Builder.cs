eb653ddfb703e6e6b5d31bd16b67bb4620c38e39
baemincheon <baemincheon@gmail.com>
2018-11-24 03:28:50 +0900

    EDIT Builder

--- Assets/Scripts/Builder.cs
+++ Assets/Scripts/Builder.cs
@@ -5,38 +5,41 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
-class Builder
+namespace BMC
 {
-    static string[] SCENES = FindEnabledEditorScenes();
-
-    static string APP_NAME = "YourProject";
-    static string TARGET_DIR = "target";
-
-    [MenuItem("Android")]
-    static void PerformMacOSXBuild()
+    class Builder
     {
-        string target_dir = APP_NAME + ".apk";
-        Build(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
-    }
+        static string[] SCENES = FindEnabledEditorScenes();
 
-    private static string[] FindEnabledEditorScenes()
-    {
-        List<string> EditorScenes = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        static string APP_NAME = "YourProject";
+        static string TARGET_DIR = "target";
+
+        [MenuItem("Android")]
+        public static void Build()
         {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
+            string target_dir = APP_NAME + ".apk";
+            GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
         }
-        return EditorScenes.ToArray();
-    }
 
-    static void Build(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
-    {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, build_target);
-        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options).ToString();
-        if (res.Length > 0)
+        private static string[] FindEnabledEditorScenes()
         {
-            throw new Exception("BuildPlayer failure : " + res);
+            List<string> EditorScenes = new List<string>();
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+            {
+                if (!scene.enabled) continue;
+                EditorScenes.Add(scene.path);
+            }
+            return EditorScenes.ToArray();
+        }
+
+        private static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+        {
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, build_target);
+            string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options).ToString();
+            if (res.Length > 0)
+            {
+                throw new Exception("BuildPlayer failure : " + res);
+            }
         }
     }
 }
